50171650ad521eead6c02074ed4e0000aabdf304
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-06-27 09:45:45 +0100

    Create head.php

---
+++ inc/head.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+
+    <title>
+        Ticketmaster: Buy Verified Tickets for Concerts, Sports, Theater and Events
+    </title>
+
+    <link
+        rel="canonical"
+        href="https://www.ticketmaster.xo.je/"
+    />
+
+    <meta
+        name="description"
+        content="Buy and sell tickets online for concerts, sports, theater, family and other events near you from Ticketmaster."
+    />
+
+    <meta property="og:type" content="website" />
+
+    <meta
+        property="og:title"
+        content="Ticketmaster: Buy Verified Tickets for Concerts, Sports, Theater and Events"
+    />
+
+    <meta
+        property="og:description"
+        content="Buy and sell tickets online for concerts, sports, theater, family and other events near you from Ticketmaster."
+    />
+
+    <meta
+        property="og:url"
+        content="https://www.ticketmaster.xo.je/"
+    />
+
+    <meta
+        property="og:site_name"
+        content="Ticketmaster US"
+    />
+
+    <meta property="og:image" content="" />
+    <meta property="og:image:width" content="720" />
+    <meta property="og:image:height" content="405" />
+
+    <meta
+        name="twitter:card"
+        content="summary_large_image"
+    />
+
+    <meta
+        name="twitter:title"
+        content="Ticketmaster: Buy Verified Tickets for Concerts, Sports, Theater and Events"
+    />
+
+    <meta
+        name="twitter:description"
+        content="Buy and sell tickets online for concerts, sports, theater, family and other events near you from Ticketmaster."
+    />
+
+    <meta
+        name="twitter:image"
+        content=""
+    />
+
+    <meta
+        name="next-head-count"
+        content="17"
+    />
+
+    <link
+        rel="icon"
+        href="//uk.tmconst.com/rc-b3c71b5a/images/favicon.ico"
+        type="image/x-icon"
+    />
+
+    <link
+        rel="shortcut icon"
+        href="//uk.tmconst.com/rc-b3c71b5a/images/favicon.ico"
+        type="image/x-icon"
+    />
+    
+    
+<link rel="stylesheet" href="assets/css/styles.css">
+
+
+    
+</head>
+<body>
+
+  <div class="sc-c6cabe26-0 bZmixf">
+   <header class="sc-9c727161-0 lgQjZq">
