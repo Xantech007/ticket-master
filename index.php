f730c7f89433615ccd23651b5ec4f6fa5cce5161
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-06-28 08:07:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,31 +5,6 @@
 <?php include "inc/navbar1.php"; ?> 
  
 <body>
-    <div id="__next">
-        <div class="sc-d727d306-0 llCpYZ">
-         Your browser is not supported. For the best experience, use any of these supported browsers:
-         <a class="Link__StyledLink-sc-pudy0l-0 bfasNL" href="https://www.google.com/chrome/">
-          Chrome
-         </a>
-         ,
-         <a class="Link__StyledLink-sc-pudy0l-0 bfasNL" href="https://www.mozilla.org/firefox/new/">
-          Firefox
-         </a>
-         ,
-         <a class="Link__StyledLink-sc-pudy0l-0 bfasNL" href="https://support.apple.com/downloads/safari">
-          Safari
-         </a>
-         ,
-         <a class="Link__StyledLink-sc-pudy0l-0 bfasNL" href="https://www.microsoft.com/edge">
-          Edge
-         </a>
-         .
-        </div>
-        <section class="sc-7f6df46b-0 frSDhw">
-         <a class="Link__StyledLink-sc-pudy0l-0 eYZQRC sc-7f6df46b-1 firzHb" href="#main-content">
-          Skip to main content
-         </a>
-        </section>    
         
 <?php include "inc/header.php"; ?>
 <?php include "body.php"; ?>        
