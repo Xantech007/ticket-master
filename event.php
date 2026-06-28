6916e64714c8d9a5b480a89de5a20616e6c82677
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-06-28 16:29:22 +0100

    Update event.php

--- event.php
+++ event.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // event.php - Top of Page Data Layer
 // Connect to database to dynamically extract artist information based on the request URL context
 require_once 'database.php';
