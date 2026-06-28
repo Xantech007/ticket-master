ef02c6067aac549e34577dd357e2448f84e5b00e
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-06-28 03:46:45 +0100

    Refactor index.php to improve structure and readability

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<?php
-// index.php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-require_once 'db.php';
-
-// Safe Database Fetch Loop
-try {
-    $stmt = $pdo->query("SELECT e.*, a.name AS artist_name, a.image AS artist_image FROM events e JOIN artists a ON e.artist_id = a.id ORDER BY e.event_date ASC LIMIT 12");
-    $dynamic_events = $stmt->fetchAll();
-} catch (Exception $e) {
-    // If your database tables aren't created yet, fallback to an empty array so the page doesn't crash
-    $dynamic_events = [];
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -25,10 +8,22 @@ try {
     <div id="__next">
         <div class="sc-d727d306-0 llCpYZ">
          Your browser is not supported. For the best experience, use any of these supported browsers:
-         <a class=\"Link__StyledLink-sc-pudy0l-0 bfasNL\" href=\"https://www.google.com/chrome/\">Chrome</a>, 
-         <a class=\"Link__StyledLink-sc-pudy0l-0 bfasNL\" href=\"https://www.mozilla.org/firefox/new/\">Firefox</a>, 
-         <a class=\"Link__StyledLink-sc-pudy0l-0 bfasNL\" href=\"https://support.apple.com/downloads/safari\">Safari</a>, 
-         <a class=\"Link__StyledLink-sc-pudy0l-0 bfasNL\" href=\"https://www.microsoft.com/edge\">Edge</a>.
+         <a class="Link__StyledLink-sc-pudy0l-0 bfasNL" href="https://www.google.com/chrome/">
+          Chrome
+         </a>
+         ,
+         <a class="Link__StyledLink-sc-pudy0l-0 bfasNL" href="https://www.mozilla.org/firefox/new/">
+          Firefox
+         </a>
+         ,
+         <a class="Link__StyledLink-sc-pudy0l-0 bfasNL" href="https://support.apple.com/downloads/safari">
+          Safari
+         </a>
+         ,
+         <a class="Link__StyledLink-sc-pudy0l-0 bfasNL" href="https://www.microsoft.com/edge">
+          Edge
+         </a>
+         .
         </div>
         <section class="sc-7f6df46b-0 frSDhw">
          <a class="Link__StyledLink-sc-pudy0l-0 eYZQRC sc-7f6df46b-1 firzHb" href="#main-content">
@@ -36,9 +31,15 @@ try {
          </a>
         </section>    
         
-        <?php include "inc/header.php"; ?>
-        <?php include "body.php"; ?>        
-        <?php include "inc/footer.php"; ?>
-    </div>
+<?php include "inc/header.php"; ?>
+<?php include "body.php"; ?>        
+<?php include "inc/footer.php"; ?>
+
+</div>
+    
+<?php include "inc/script.php"; ?>
+
+<div id="modals" data-testid="modals">
+</div>
 </body>
 </html>
