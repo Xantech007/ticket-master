66eb66e1e9e331d507455fea6903c1e5b5aa9a3d
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-06-29 07:59:37 +0100

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,25 +1,26 @@
 <?php
+
 class Database {
+
     private $host = "sql207.infinityfree.com";
     private $dbname = "if0_42273705_ticket2";
     private $username = "if0_42273705";
     private $password = "MWJvmCfpNDKo";
 
-    public function connect(){
-        try {
-            $pdo = new PDO(
-                "mysql:host={$this->host};dbname={$this->dbname};charset=utf8mb4",
-                $this->username,
-                $this->password,
-                [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-                ]
-            );
-            return $pdo;
-        } catch(PDOException $e){
-            die("Database connection failed: " . $e->getMessage());
-        }
+    public function connect() {
+
+        return new PDO(
+            "mysql:host={$this->host};dbname={$this->dbname};charset=utf8mb4",
+            $this->username,
+            $this->password,
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]
+        );
     }
 }
+
+$database = new Database();
+$pdo = $database->connect();
 ?>
