d8828afc77f9c4e43389810338fe1c6b893d7bda
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-06-29 20:51:17 +0100

    Update events.php

--- events.php
+++ events.php
@@ -5,9 +5,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// 1. Corrected Path: Load the connection file from the 'config' folder
-require_once 'config/db.php';
-
 // 2. Instantiate your "Database" class and invoke the connect() function to expose $pdo safely
 $pdo = null;
 try {
