7c7aa9fce3ffde5f83f65d0d0327bc4f24c81b81
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-06-29 23:26:33 +0100

    Update body.php

--- body.php
+++ body.php
@@ -1,4 +1,10 @@
 <?php
+
+// Enable error displaying so we can pinpoint issues if database structural details are missing
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 
 /*
