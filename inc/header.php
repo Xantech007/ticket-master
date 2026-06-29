1407a91dcb8904e136044ad4e6f840c6041d70c8
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-06-29 20:52:46 +0100

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -1,6 +1,6 @@
-<?php include "../config/db.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
+  <?php include "../config/db.php"; ?>
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
