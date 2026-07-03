4603dccdecaa1c28adad441db985119a4de116de
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-07-03 22:20:35 +0100

    Delete inc/session.php

--- inc/session.php
+++
@@ -1,7 +0,0 @@
-// inc/header.php - Global Navigation Component with Session-Safe Initialization
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-// Check if a user is actively authenticated to toggle view state layouts
-$is_logged_in = isset($_SESSION['user_id']);
