156e1fa857677ea81f09ecbff8fe59d1c9c31f2c
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-07-03 23:32:17 +0100

    Update dashboard.php

--- auth/dashboard.php
+++ auth/dashboard.php
@@ -5,6 +5,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require_once '../config/db.php';
+
 if (!isset($_SESSION['user_id'])) {
 
     $_SESSION['auth_error'] = "Please login to continue checkout.";
@@ -15,9 +17,6 @@ if (!isset($_SESSION['user_id'])) {
 }
 
 $user_id = (int) $_SESSION['user_id'];
-
-// 1. Initialize access validations & database components
-require_once '../config/db.php';
 
 $pdo = null;
 try {
