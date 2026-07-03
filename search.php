7a7a8d125d47c5e07ba3351fa2d60d2984bfc0e2
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-07-03 10:43:26 +0100

    Update search.php

--- search.php
+++ search.php
@@ -1,13 +1,7 @@
 <?php
-session_start();
 
-if (!isset($_SESSION['user_id'])) {
-
-    $_SESSION['auth_error'] = "Please login to continue booking your seats.";
-    $_SESSION['redirect_after_auth'] = $_SERVER['REQUEST_URI'];
-
-    header("Location: auth.php");
-    exit;
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
 }
 
 $pdo = null;
