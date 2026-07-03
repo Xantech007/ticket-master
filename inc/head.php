62ba8a63adda6aaa2ee6d10456eb3c43749ef12b
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-07-03 06:16:10 +0100

    Update head.php

--- inc/head.php
+++ inc/head.php
@@ -1,83 +1,33 @@
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <title>
-        Ticketmaster: Buy Verified Tickets for Concerts, Sports, Theater and Events
-    </title>
+    <title>Ticketmaster: Buy Verified Tickets for Concerts, Sports, Theater and Events</title>
 
-    <link
-        rel="canonical"
-        href="https://www.ticketmaster.xo.je/"
-    />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
 
-    <meta
-        name="description"
-        content="Buy and sell tickets online for concerts, sports, theater, family and other events near you from Ticketmaster."
-    />
+    <link rel="canonical" href="https://www.ticketmaster.xo.je/" />
+    <meta name="description" content="Buy and sell tickets online for concerts, sports, theater, family and other events near you from Ticketmaster." />
 
     <meta property="og:type" content="website" />
-
-    <meta
-        property="og:title"
-        content="Ticketmaster: Buy Verified Tickets for Concerts, Sports, Theater and Events"
-    />
-
-    <meta
-        property="og:description"
-        content="Buy and sell tickets online for concerts, sports, theater, family and other events near you from Ticketmaster."
-    />
-
-    <meta
-        property="og:url"
-        content="https://www.ticketmaster.xo.je/"
-    />
-
-    <meta
-        property="og:site_name"
-        content="Ticketmaster US"
-    />
-
+    <meta property="og:title" content="Ticketmaster: Buy Verified Tickets for Concerts, Sports, Theater and Events" />
+    <meta property="og:description" content="Buy and sell tickets online for concerts, sports, theater, family and other events near you from Ticketmaster." />
+    <meta property="og:url" content="https://www.ticketmaster.xo.je/" />
+    <meta property="og:site_name" content="Ticketmaster US" />
     <meta property="og:image" content="" />
     <meta property="og:image:width" content="720" />
     <meta property="og:image:height" content="405" />
 
-    <meta
-        name="twitter:card"
-        content="summary_large_image"
-    />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Ticketmaster: Buy Verified Tickets for Concerts, Sports, Theater and Events" />
+    <meta name="twitter:description" content="Buy and sell tickets online for concerts, sports, theater, family and other events near you from Ticketmaster." />
+    <meta name="twitter:image" content="" />
 
-    <meta
-        name="twitter:title"
-        content="Ticketmaster: Buy Verified Tickets for Concerts, Sports, Theater and Events"
-    />
-
-    <meta
-        name="twitter:description"
-        content="Buy and sell tickets online for concerts, sports, theater, family and other events near you from Ticketmaster."
-    />
-
-    <meta
-        name="twitter:image"
-        content=""
-    />
-
-    <meta
-        name="next-head-count"
-        content="17"
-    />
-
-    <link
-        rel="icon"
-        href="//uk.tmconst.com/rc-b3c71b5a/images/favicon.ico"
-        type="image/x-icon"
-    />
-
-    <link
-        rel="shortcut icon"
-        href="//uk.tmconst.com/rc-b3c71b5a/images/favicon.ico"
-        type="image/x-icon"
-    />
+    <meta name="next-head-count" content="17" />
+    <link rel="icon" href="//uk.tmconst.com/rc-b3c71b5a/images/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="//uk.tmconst.com/rc-b3c71b5a/images/favicon.ico" type="image/x-icon" />
     
     <script type="text/plain" src="/eps-mgr" async="" class="optanon-category-C0001 external-scripts"></script>
     <script type="text/plain" async="" class="optanon-category-C0001 external-scripts">(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K4QMLG');</script>
@@ -95,45 +45,15 @@
     <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/framework-3bc9684f49160ebb.js" defer="" crossorigin="anonymous"></script>
     <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/main-233f68431b39018e.js" defer="" crossorigin="anonymous"></script>
     <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/pages/_app-3c5067e2872ee16d.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/8023-53436e687220d7b7.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/762-ae69ee2acf6d5057.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/7424-5f7ec3b931ba997e.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/7382-b66c0fb1faa8018f.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/2193-b5af5bc43155145c.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/6629-7473d9ac7885e983.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/3800-1a42bb5b97cc5e94.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/1192-e8d7fe564112ce11.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/1412-8873dbe44d9444d2.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/7530-c89099a7c5fac8de.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/59-d4679c098740fada.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/267-a7b84725e24b227a.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/5097-33d7585c22d4ded3.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/5698-d4f0b9e8fa2ccc95.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/1857-675908198e58f226.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/6282-bcfe293495be70c7.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/2958-f19f013b18fb504b.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/4104-a591431c8bff958b.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/5870-15c158f49c7ade07.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/1531-b04e211d65e9f2bf.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/6586-b24531a1f4baad3f.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/4686-98a6ba77732c8f9e.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/2517-378760b9eeff4b80.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/132-50ab825791cc8ce9.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/6365-6058d6410dc1bc1a.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/5645-4c4e4e073f893a98.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/9475-c690661aa7b1c087.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/chunks/pages/index-944553b101bc26d2.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/DjlKq6_3BMog0qEZLQuM2/_buildManifest.js" defer="" crossorigin="anonymous"></script>
-    <script src="//uk.tmconst.com/rc-b3c71b5a/_next/static/DjlKq6_3BMog0qEZLQuM2/_ssgManifest.js" defer="" crossorigin="anonymous"></script>
     
-    <link rel="stylesheet" href="assets/css/styles.css">
-
     <div id="google_translate_element" style="display:none !important;"></div>
     <style>
         .goog-te-banner-frame, .goog-te-gadget, .skiptranslate, #goog-gt-tt, .goog-te-balloon-frame { 
             display: none !important; 
         }
         body { top: 0px !important; position: static !important; }
+        .nav-link { transition: color 0.2s ease; }
+        .nav-link:hover { color: #fff; }
     </style>
     <script type="text/javascript">
         function googleTranslateElementInit() {
@@ -143,7 +63,6 @@
                 autoDisplay: false
             }, 'google_translate_element');
             
-            // Check cookie loop to re-apply the correct language settings on load
             setTimeout(function() {
                 let match = document.cookie.match(new RegExp('(^| )googtrans=([^;]+)'));
                 if (match) {
@@ -159,4 +78,4 @@
         }
     </script>
     <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-    </head>
+</head>
