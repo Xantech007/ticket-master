80498b6614fbab06d2665835424c2b3daee607aa
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-07-03 23:29:05 +0100

    Update dashboard.php

--- auth/dashboard.php
+++ auth/dashboard.php
@@ -1,5 +1,6 @@
 <?php
-// dashboard.php - Secured User Command Center Portfolio Node
+session_start();
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
