01f9fba3a02974d4ee87f9fae3348e8d62eb1fd8
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-07-05 15:12:46 +0100

    Update navbar.php

--- inc/navbar.php
+++ inc/navbar.php
@@ -60,7 +60,7 @@
 
         <a href="#" class="nav-link">Sell</a>
 
-        <a href="#" class="nav-link flex items-center gap-1.5">
+        <a href="/giftcard.php" class="nav-link flex items-center gap-1.5">
           <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-1a2 2 0 01-2-2H9a2 2 0 01-2-2v-1a2 2 0 012-2m0 0V9a2 2 0 012-2" />
           </svg>
